60b266b26f8f74e9d09f9af9d8205d7e34e7392d
Vishruth Devan <vishruthdevan@gmail.com>
2022-12-24 21:26:47 +0530

    feat: generate a new password until successful

--- main_test.go
+++ main_test.go
@@ -6,10 +6,17 @@ import (
 )
 
 func TestHash(t *testing.T) {
-	var cracked, tries = password.CrackPass("password")
-	if cracked {
-		t.Logf("success | number of tries: %v", tries)
-	} else {
-		t.Errorf("failure | could not crack password")
+	password.SetSeed()
+	count := 1000000
+	for i := 0; i < count; i++ {
+		var cracked, testPass = password.CrackPass("qwerty")
+		if cracked {
+			t.Logf("success | number of tries: %v", i+1)
+			t.Logf("generated password: %v", testPass)
+			break
+		} else {
+			t.Errorf("failure | could not crack password after %v tries", i+1)
+			t.Errorf("generated password: %v", testPass)
+		}
 	}
 }
